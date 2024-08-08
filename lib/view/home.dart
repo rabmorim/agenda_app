677a501d0929fe37795f8d97bad77a06fd33382a
rabmorim <ramorim13@yahoo.com.br>
2024-08-08 15:51:43 -0300

    Implementação na agenda

--- lib/view/home.dart
+++ lib/view/home.dart
@@ -2,37 +2,87 @@ import 'package:agenda_app/view/recursos/barra_superior.dart';
 import 'package:agenda_app/view/recursos/drawer.dart';
 import 'package:flutter/material.dart';
 
+
 class Home extends StatefulWidget {
   const Home({super.key});
-  
+
   @override
   State createState() => HomeState();
-    
 }
 
 class HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-   return Scaffold(
-    
-    //appbar
+    return Scaffold(
+      //appbar
 
-     appBar: BarraSuperior() ,
+      appBar: BarraSuperior(),
 
-    //drawer
+      //drawer
 
-    drawer: const MenuDrawer() ,
-    //body
+      drawer: const MenuDrawer(),
 
-    // body: ,
+      //body
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+                padding: const EdgeInsets.all(25),
+                child: Center(
+                  child: Column(
+                    children: [
+                      const Padding(padding: EdgeInsets.only(bottom: 100)),
+                      const Text(
+                        "Bem vindo ao Conecta+",
+                        style: TextStyle(
+                            color: Colors.white54,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 28,
+                            letterSpacing: BorderSide.strokeAlignCenter),
+                      ),
+                      const Padding(padding: EdgeInsets.only(bottom: 80)),
+                      //Botao Buscar Contato
+                      Builder(builder: (BuildContext context) {
+                        return ElevatedButton(
+                          onPressed: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => const Home()));
+                          },
+                          child: Container(
+                            width: 150,
+                            padding: const EdgeInsets.all(10),
+                            child: const Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Icon(
+                                  Icons.search,
+                                  color: Colors.white54,
+                                ),
+                                Text(
+                                  "Buscar Contatos",
+                                  style: TextStyle(
+                                    color: Colors.white54,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        );
+                      }),
+                      //Botão cadastrar contato
+                    ],
+                  ),
+                ))
+          ],
+        ),
+      ),
 
-    //Bottom navigation 
+      //Bottom navigation
 
-    // bottomNavigationBar: ,
-
-
-
-   );
+      // bottomNavigationBar: ,
+    );
   }
-
 }
--- lib/view/recursos/drawer.dart
+++ lib/view/recursos/drawer.dart
@@ -1,3 +1,4 @@
+import 'package:agenda_app/view/home.dart';
 import 'package:flutter/material.dart';
 
 class MenuDrawer extends StatelessWidget{
@@ -6,6 +7,25 @@ class MenuDrawer extends StatelessWidget{
   final usuario = "Rodrigo Abreu Amorim";
   final email = "Teste@gmail.com";
   final fotoPerfil = "img/perfil.png";
+
+  Text mostrarTitulo (String texto) {
+    return Text(
+      texto,
+      style: const TextStyle(
+        color: Colors.white54,
+        fontWeight: FontWeight.bold
+      ),
+    );
+  }
+
+  Text mostrarSubTitulo(String texto) {
+    return Text(
+      texto,
+      style: const  TextStyle(
+        color: Colors.white54
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -37,12 +57,99 @@ class MenuDrawer extends StatelessWidget{
               ),
 
               //Home
+             ListTile(
+              title: mostrarTitulo('Home') ,
+              subtitle: mostrarSubTitulo('Página Inicial') ,
+              trailing: const Icon(
+                Icons.keyboard_arrow_right,
+                 color: Colors.white54 ,
+                ),
+              leading: const Icon(
+                Icons.home,
+                size: 28,
+              ),
+              //Vai para a página home
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const Home()
+                    )
+                  );
+              },
+            ),
+            
+            //Perfil
+            ListTile(
+               title: mostrarTitulo('Perfil') ,
+               subtitle: mostrarSubTitulo('Meu perfil'),
+              trailing: const Icon(
+                Icons.keyboard_arrow_right,
+                 color: Colors.white54 ,
+                ),
+              leading: const Icon(
+                Icons.person,
+                size: 28,
+              ),
+              //Vai para página de Perfil
+              onTap: () {
+                
+              },
+            ),
 
-              //Meus contatos
 
-              //COnfigurações
 
-              //Logout
+             //Meus contatos
+             ListTile(
+               title: mostrarTitulo('Contatos') ,
+              subtitle: mostrarSubTitulo('Meus Contatos') ,
+              trailing: const Icon(
+                Icons.keyboard_arrow_right,
+                 color: Colors.white54 ,
+                ),
+              leading: const Icon(
+                Icons.contact_page,
+                size: 28,
+              ),
+              //Vai para pagina de contatos
+              onTap: () {
+                
+              },
+            ),
+
+            //Configurações
+            ListTile(
+               title: mostrarTitulo('Configurações') ,
+              trailing: const Icon(
+                Icons.keyboard_arrow_right,
+                 color: Colors.white54 ,
+                ),
+              leading: const Icon(
+                Icons.settings,
+                size: 28,
+              ),
+              //Vai para página de Configurações
+              onTap: () {
+                
+              },
+            ),
+
+            //Logout
+            ListTile(
+               title: mostrarTitulo('Logout') ,
+              subtitle: mostrarSubTitulo('Sair') ,
+              trailing: const Icon(
+                Icons.keyboard_arrow_right,
+                 color: Colors.white54 ,
+                ),
+              leading: const Icon(
+                Icons.logout,
+                size: 28,
+              ),
+              onTap: () {
+                
+              },
+            )
 
         ],
       ),
